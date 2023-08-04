7d6490fccf03871709258ff53de9bceccbd4a466
Vladimir Solovyov <svn131131@gmail.com>
2023-08-05 00:50:50 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+    public class Main {
+        public static void main(String[] args) {
+            List<Integer> arrayList = new ArrayList<>();
+
+            for (int i = 0; i <= 10; i++) {
+                arrayList.add(i);
+            }
+
+// Получение размера списка
+            int size1 = arrayList.size();
+            System.out.println("Размер списка: " + size1);
+
+            arrayList.remove(5);
+
+            System.out.println(arrayList);
+
+
+
+            // Получение размера списка
+            int size = arrayList.size();
+            System.out.println("Размер списка: " + size);
+
+            System.out.println("getCapacity");
+            getArrayListCapacity(arrayList);
+        }
+
+        private static void getArrayListCapacity(List<?> arrayList) {
+            try {
+                Field dataField = ArrayList.class.getDeclaredField("elementData");
+                dataField.setAccessible(true);
+                Object[] elementData = (Object[]) dataField.get(arrayList);
+                int capacity = elementData.length;
+                System.out.println("Емкость списка: " + capacity);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
